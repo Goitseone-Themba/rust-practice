6f8532105e0e413aa5357f268110cf1d4f86a11e
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-09-12 02:21:07 +0200

    variables, mutability

---
+++ variables/target/debug/deps/variables-87cb5ec98b5133fc.d
@@ -0,0 +1,5 @@
+/home/goitseone/Projects/Rust/rust-practice/variables/target/debug/deps/variables-87cb5ec98b5133fc: src/main.rs
+
+/home/goitseone/Projects/Rust/rust-practice/variables/target/debug/deps/variables-87cb5ec98b5133fc.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/goitseone/Projects/Rust/rust-practice/variables/target/debug/variables: /home/goitseone/Projects/Rust/rust-practice/variables/src/main.rs
