fbcca0dde0b132964f9d42255f566b0da4fda415
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-04-02 01:18:28 +0200

    Axum framework, helloworld with Get and Redirect

---
+++ axum_server/src/main.rs
@@ -0,0 +1,25 @@
+use axum::{response::Redirect, routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new()
+        .route("/hello-world", get(hello_world))
+        .fallback(anything_else);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+async fn anything_else() -> Redirect {
+    Redirect::to("/hello-world")
+}
